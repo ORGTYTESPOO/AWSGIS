ec00d4abb3acc40e0f9ec96709230e9fcb72e355
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-01-24 14:14:52 +0200

    Add street and street condition layers on the map and change basemap to OSM

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,12 +11,33 @@ export class MapComponent {
   constructor() { }
 
   ngOnInit() {
-    var map = L.map(document.getElementById('map')).setView([60, 24], 9);
+    var map = L.map(document.getElementById('map')).setView([60.23, 24.8], 13);
 
-    var wmsLayer = L.tileLayer.wms('http://demo.opengeo.org/geoserver/ows?', {
-      layers: 'ne:ne'
+    var basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-  }
+    var streetLayer = new L.WFST({
+      url: 'http://geoserver-lb-1359047372.eu-west-1.elb.amazonaws.com/geoserver/espoo/ows',
+      typeNS: 'espoo',
+      typeName: 'katu',
+      geometryField: 'geom',
+      style: {
+        color: 'blue',
+        weight: 2
+      }
+    }).addTo(map);
 
+    var streetConditionLayer = new L.WFST({
+      url: 'http://geoserver-lb-1359047372.eu-west-1.elb.amazonaws.com/geoserver/espoo/ows',
+      typeNS: 'espoo',
+      typeName: 'katualueet_kuntotieto',
+      geometryField: 'geom',
+      style: {
+        color: 'red',
+        weight: 3
+      }
+    }).addTo(map);
+  }
 }
