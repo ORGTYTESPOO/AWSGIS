fe5a424d9cbb62b7685241786c5534a212e0f4df
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2017-01-26 13:17:53 +0200

    Replace Leaflet with Openlayers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-declare var L: any;
+declare var ol: any;
 
 @Component({
   selector: 'esp-map',
@@ -13,12 +13,32 @@ export class MapComponent {
   constructor() { }
 
   ngOnInit() {
-    this.map = L.map(document.getElementById('map')).setView([60.23, 24.8], 13);
-
-    var basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
+    // this.map = L.map(document.getElementById('map')).setView([60.23, 24.8], 13);
+    //
+    // var basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   maxZoom: 19,
+    //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // }).addTo(this.map);
+  //
+    // Create map with OSM base layer and the marker layer
+    var map = new ol.Map({
+      controls: ol.control.defaults({
+        attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+          collapsible: false
+        })
+      }),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      target: 'map',
+      view: new ol.View({
+        projection: 'EPSG:900913',
+        center: [18.0, 55.4],
+        zoom: 7
+      })
+    });
 
   }
 }
