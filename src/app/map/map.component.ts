34847aa8549458e1f5a226838b0bc6094fb11122
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2017-02-02 10:24:34 +0200

    Add buttons to change theme

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,7 +15,6 @@ export class MapComponent {
   constructor() { }
 
   ngOnInit() {
-    console.log(ol);
 
     const centerLongitude = 24.82;
     const centerLatitude = 60.228;
@@ -32,33 +31,86 @@ export class MapComponent {
 
     if(isMobile.any && navigator.geolocation) {
       navigator.geolocation.getCurrentPosition( (result) => {
-        centerCoordinate = ol.proj.fromLonLat([result.coords.longitude, result.coords.latitude])
-        this.showMap = true;
+        centerCoordinate = ol.proj.fromLonLat([result.coords.longitude, result.coords.latitude]);
+
         const geolocatedMapConfig = {
           target: 'map',
           view: new ol.View({
             center: centerCoordinate,
             zoom: 17
           })
-        }
-        this.map = new ol.Map(geolocatedMapConfig);
-        this.map.addLayer(basemapLayer);
+        };
+        this.initializeMap(geolocatedMapConfig, basemapLayer);
       }, () => {
-        this.showMap = true;
-        this.map = new ol.Map(defaultMapConfig);
-        this.map.addLayer(basemapLayer);
+        this.initializeMap(defaultMapConfig, basemapLayer);
       });
     } else {
-      this.showMap = true;
-      this.map = new ol.Map(defaultMapConfig);
-      this.map.addLayer(basemapLayer);
+      this.initializeMap(defaultMapConfig, basemapLayer);
     }
 
-
-
-
-
-
-
   }
+
+  initializeMap(mapConfig: any, basemapLayer: any) {
+    this.showMap = true;
+    mapConfig.controls = ol.control.defaults().extend([ this.getStreetMaintenanceControl(), this.getStreetConditionControl() ]);
+    this.map = new ol.Map(mapConfig);
+    this.map.addLayer(basemapLayer);
+  }
+
+  getStreetMaintenanceControl() {
+    let streetTypeSelectionControlConfiguration = function(opt_options): void {
+
+      let options = opt_options || {};
+
+      let button = document.createElement('button');
+      button.innerHTML = 'YT';
+
+      button.addEventListener('click', () => {
+        // TODO CHANGE THEME
+        console.log('Change maintenance theme');
+      }, false);
+
+      let element = document.getElementById('control');
+      element.className = 'street-information ol-control';
+      element.appendChild(button);
+
+      ol.control.Control.call(this, {
+        element: element,
+        target: options.target
+      });
+
+    };
+    ol.inherits(streetTypeSelectionControlConfiguration, ol.control.Control);
+
+    return new streetTypeSelectionControlConfiguration({target: 'map'});
+  }
+
+  getStreetConditionControl() {
+    let streetTypeSelectionControlConfiguration = function(opt_options): void {
+
+      let options = opt_options || {};
+
+      let button = document.createElement('button');
+      button.innerHTML = 'KT';
+
+      button.addEventListener('click', () => {
+        // TODO CHANGE THEME
+        console.log('Change condition theme');
+      }, false);
+
+      let element = document.getElementById('control');
+      element.className = 'street-information ol-control';
+      element.appendChild(button);
+
+      ol.control.Control.call(this, {
+        element: element,
+        target: options.target
+      });
+
+    };
+    ol.inherits(streetTypeSelectionControlConfiguration, ol.control.Control);
+
+    return new streetTypeSelectionControlConfiguration({target: 'map'});
+  }
+
 }
