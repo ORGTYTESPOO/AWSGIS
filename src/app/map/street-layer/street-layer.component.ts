36e0bc22d4251570061c260b8d27ebbb1c96a538
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-02-03 16:33:16 +0200

    Handle updates through a layer which is directly connected to database table instead of view

--- src/app/map/street-layer/street-layer.component.ts
+++ src/app/map/street-layer/street-layer.component.ts
@@ -15,6 +15,7 @@ export class StreetLayerComponent implements OnInit {
   dialogParameterStream: Subject<any>;
   @Input() layerSelectionActionStream: Subject<string>;
   wmsSource: ol.source.TileWMS;
+  updateSource: ol.source.TileWMS;
 
   constructor() { }
 
@@ -47,6 +48,16 @@ export class StreetLayerComponent implements OnInit {
       source: this.wmsSource
     });
 
+    this.updateSource = new ol.source.TileWMS({
+      // url: 'http://localhost:8080/geoserver/espoo/wms',
+      url: 'http://geoserver-lb-1359047372.eu-west-1.elb.amazonaws.com/geoserver/espoo/wms',
+      params: {
+        'LAYERS': 'katu',
+        'TILED': true
+      },
+      serverType: 'geoserver'
+    });
+
     this.map.on('click', (e) => {
       let resolution = this.map.getView().getResolution();
       let projection = 'EPSG:3857';
@@ -54,7 +65,7 @@ export class StreetLayerComponent implements OnInit {
         INFO_FORMAT: 'application/json'
       };
 
-      let url = this.wmsSource.getGetFeatureInfoUrl(
+      let url = this.updateSource.getGetFeatureInfoUrl(
         e.coordinate,
         resolution,
         projection,
