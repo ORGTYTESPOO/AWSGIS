635e8199e0113f009d47a413c1df091c375c7b64
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-09-12 21:20:54 +0300

    add mimetypes

--- lambda-proxy/lambda.js
+++ lambda-proxy/lambda.js
@@ -1,6 +1,14 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
-const server = awsServerlessExpress.createServer(app);
+
+const binaryMimeTypes = [
+  'image/jpeg',
+  'image/png',
+  'image/apng',
+  'image/webp',
+];
+
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
 exports.handler = (event, context) =>
   awsServerlessExpress.proxy(server, event, context);
